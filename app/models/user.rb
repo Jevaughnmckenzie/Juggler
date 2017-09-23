65a71c1fcc351a58c73e48766ee22b40d341e645
jevaughnmckenzie <jmckenzie1004@gmail.com>
2017-09-23 17:05:50 -0400

    add validations for username and password in User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   has_many :tasks, through: :projects
 
   has_secure_password
+
+  validates :username, { presence: true, uniqueness: true }
+  validates :password, { presence: true, length: { minimum: 6 } }
+
 end
