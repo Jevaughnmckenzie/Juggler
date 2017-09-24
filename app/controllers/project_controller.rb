1cfa35805c5d069ba46fa4740a2bec0c4d944535
jevaughnmckenzie <jmckenzie1004@gmail.com>
2017-09-24 16:46:00 -0400

    create 'projects/edit' view and corresponding controller actions

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -35,5 +35,28 @@ class ProjectController < ApplicationController
 
   	redirect '/projects'
   end
+
+  get '/projects/:id/edit' do
+  	if logged_in?
+  		
+      erb :'projects/edit'
+    else
+    	redirect '/'
+    end
+  end
+
+  patch '/projects/:id/edit' do
+  	project = Project.find(params[:id])
+
+  	project.name = params[:name]
+  	params[:active] == "on" ? project.active == true : project.active == false
+
+  	if project.save
+  		redirect "/projects/#{project.id}"
+  	else
+  		# Set up error message
+  		erb :'projects/edit'
+  	end
+  end
   
 end
