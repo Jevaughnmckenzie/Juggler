f780aad9e70caab1048ed1fa01f0b78af1bc9de2
jevaughnmckenzie <jmckenzie1004@gmail.com>
2017-09-24 14:59:42 -0400

    prevent access to the 'projects' page if user is not logged in

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -2,7 +2,13 @@ require './config/environment'
 class ProjectController < ApplicationController
   
   get '/projects' do
-    erb :'projects/index'
+  	if logged_in?
+  	  user = User.find(current_user)
+      @projects = user.projects
+      erb :'projects/index'
+    else
+  	  redirect '/'
+    end
   end
   
 end
